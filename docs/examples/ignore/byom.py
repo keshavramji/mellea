5a2aed4df4035ed32869584645ca3af32bc5a9f6
Nathan Fulton <nathan@ibm.com>
2025-08-06 11:39:17 -0400

    Some additional cleanup.

--- docs/examples/ignore/byom.py
+++
@@ -1,9 +0,0 @@
-from mellea import MelleaSession
-from mellea.backends.ollama import OllamaModelBackend
-from mellea.stdlib.base import LinearContext
-
-m = MelleaSession(
-    backend=OllamaModelBackend(model_id="llama3.2:latest"), ctx=LinearContext()
-)
-
-print(m.chat("Hi, meta! Tell me about Mark Zuckerberg."))
