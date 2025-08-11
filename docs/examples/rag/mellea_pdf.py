d33f767ad32fe9a6d2a3d0c5ef6d15f764f6685c
Nathan Fulton <nathan@ibm.com>
2025-08-11 15:15:04 -0600

    Adds a simple Mellea example. (#35)

---
+++ docs/examples/rag/mellea_pdf.py
@@ -0,0 +1,15 @@
+import mellea
+from mellea.stdlib.docs.richdocument import RichDocument
+
+m = mellea.start_session()
+
+doc = RichDocument.from_document_file(
+    "https://www.fs.usda.gov/foresthealth/docs/fidls/FIDL-78-ArmillariaRootDisease.pdf"
+)
+
+print(
+    m.instruct(
+        "Determine whether the mellea fungus only feeds on dead or weakened trees, thereby improving forest health",
+        grounding_context={"usfs": doc},
+    ).value
+)
